2f380cc88df98151a7fd12d8b1ece50e94da0c2e
Charles Rollin <charles.rollin@student.ecp.fr>
2017-02-26 23:14:23 +0100

    Make `split_docs.py` both callable and importable

--- split_docs.py
+++ split_docs.py
@@ -12,23 +12,29 @@ def is_marker(line_begin):
 def is_interesting_marker(line_begin):
     return line_begin in interesting_markers
 
-doc_counter = 0
-storing = False
-temp = ""
-with open("cacm.all") as cacm_file:
-    for line in cacm_file:
-        line_begin = line[0:2]
-        if not is_marker(line_begin):
-            if storing:
-                for word in filter(None, re.split('[^a-zA-Z\d:]', line)):
-                    temp += word.lower() + " "
-        else:
-            if is_interesting_marker(line_begin):
-                if line_begin == '.I' and temp != "":
-                    doc_counter += 1
-                    with open("cacm-data/0/CACM-%s.html" % doc_counter, 'w') as doc_file:
-                        doc_file.write(temp)
-                    temp = ""
-                storing = True
+
+def split():
+    doc_counter = 0
+    storing = False
+    temp = ""
+    with open("cacm.all") as cacm_file:
+        for line in cacm_file:
+            line_begin = line[0:2]
+            if not is_marker(line_begin):
+                if storing:
+                    for word in filter(None, re.split('[^a-zA-Z\d:]', line)):
+                        temp += word.lower() + " "
             else:
-                storing = False
+                if is_interesting_marker(line_begin):
+                    if line_begin == '.I' and temp != "":
+                        doc_counter += 1
+                        with open("cacm-data/0/CACM-%s.html" % doc_counter, 'w') as doc_file:
+                            doc_file.write(temp)
+                        temp = ""
+                    storing = True
+                else:
+                    storing = False
+
+
+if __name__ == '__main__':
+    split()
